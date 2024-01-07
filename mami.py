062d7b48f28ac087ac7389244bad1437c0c349b1
Katilmami <akbalmuhammed668@gmail.com>
2024-01-07 23:35:08 +0300

    Update mami.py

--- mami.py
+++ mami.py
@@ -5,7 +5,7 @@ def run_background_command(command):
     subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 # Windows'ta color komutu kullanma örneği
-color_command = 'color 0a'  # 0a renk kodu, siyah zemin üzerine yeşil yazı demektir
+color_command = 'color a'  # 0a renk kodu, siyah zemin üzerine yeşil yazı demektir
 run_background_command(color_command)
 
 # Programın durmaması için bir bekleme ekleyebilirsiniz
