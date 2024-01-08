1d23f66e274c4f343c90ed786a5afc125741e169
Katilmami <akbalmuhammed668@gmail.com>
2024-01-08 19:32:13 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ import py2exe
 
 setup(
     options={'py2exe': {'bundle_files': 1, 'compressed': True}},
-    windows=[{'script': 'mami.py'}],  # your_script.py, ana Python betiğinizin adını belirtin
+    windows=[{'script': 'wiamtool.py'}], 
     zipfile=None,
 )
